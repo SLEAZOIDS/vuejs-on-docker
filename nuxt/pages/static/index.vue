db7482014b02aed1c21641beaa4fced3f45853b4
SLEAZOIDS <vegetarian304@gmail.com>
2018-08-10 19:21:38 +0900

    static読み込み

---
+++ nuxt/pages/static/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">stacic test page</div>
+</template>
+
+<script>
+export default {
+  head () {
+    return {
+      script: [
+        { src: "/sample.js" }
+      ],
+      link: [
+        { rel: 'stylesheet', href: "/sample.css" }
+      ]
+    }
+  }
+}
+</script>
